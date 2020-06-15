844f38b0b6501483e049e7af290a5aa5202d476a
sirbaconsalot <donavynhaley@gmail.com>
2020-06-14 20:07:12 -0700

    Finished Sprint MVP and all Stretch Goals

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,16 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+/* The nested function can access the initernal variable because it is enclosed within the parent function. Similar to the way myFunction can call from variables on the global scope. */
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation(number){
+  let total = 0;
+  for(let i=0; i < number; i++){
+    total+= i+1;
+  }
+  return total;
+}
+console.log(summation(4));
